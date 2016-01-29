d5c75a8cb2e7e6bb7de9d7ccb42f6dc2fb301b7b
Venkatesh Jujjavarapu <venkatesh7j@gmail.com>
2016-01-28 21:11:20 -0500

    Initial Commit

---
+++ EvilStudiosCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EvilStudiosCode
+//
+//  Created by Venkatesh Jujjavarapu on 1/28/16.
+//  Copyright © 2016 Venkatesh Jujjavarapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EvilStudiosCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EvilStudiosCode
+//
+//  Created by Venkatesh Jujjavarapu on 1/28/16.
+//  Copyright © 2016 Venkatesh Jujjavarapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
